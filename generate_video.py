205c5e16b7a72dcfdc966c8f210a78d519bcb412
Ulzee An <ul1994@gmail.com>
2018-07-17 22:42:46 -0400

    much higher zoom for images

--- generate_video.py
+++ generate_video.py
@@ -32,6 +32,7 @@ def gather_videos(SEQ_LEN = 4, still=False):
 		xs, ys, bbox = mat['x'], mat['y'], mat['bbox']
 		vis, dim = mat['visibility'], mat['dimensions']
 
+		# for bii in range(0, len(imgs), SEQ_LEN):
 		for bii in range(0, len(imgs), SEQ_LEN):
 			ref = {
 				'frames': [],
@@ -95,33 +96,42 @@ import cv2
 from cv2 import imread
 import numpy as np
 import random
-def size_image(img, size=368):
+def size_image(img, bbox, size=368):
 	img = img.astype(np.float32)
 
-	maxdim = max(img.shape[:2])
-	ratio = size / maxdim
+	# zoom = 1 / random.uniform(1.25, 1.75) # x1.5 ~ x2.0
+	zoom = 1 / random.uniform(1.0, 1.25) # x1.5 ~ x2.0
+	targ = size * zoom
+	half = targ / 2
 
-	img = cv2.resize(img, (0,0), fx=ratio, fy=ratio)
+	cx, cy = (bbox[2] + bbox[0]) / 2, (bbox[3] + bbox[1]) / 2
 
-	random.uniform(0.5, 0.75)
+	x0, y0 = max(0, cx - half), max(0, cy - half)
+	region = img[int(y0):int(y0+targ), int(x0):int(x0+targ)]
+	maxlen = max(region.shape[0], region.shape[1])
+
+	region = cv2.resize(region, (0,0), fx=368 / maxlen, fy=368 / maxlen)
+	assert region.shape[0] == size or region.shape[1] == size
 
 	canvas = np.zeros((size, size, 3))
+
+	# center image again b/c region is not always a square
 	dx, dy = 0, 0
-	if img.shape[0] > img.shape[1]:
-		dx = int((size - img.shape[1]) / 2)
+	if region.shape[0] > region.shape[1]:
+		dx = int((size - region.shape[1]) / 2)
 	else:
-		dy = int((size - img.shape[0]) / 2)
+		dy = int((size - region.shape[0]) / 2)
 
-	canvas[dy:dy+img.shape[0], dx:dx+img.shape[1]] = img
+	canvas[dy:dy+region.shape[0], dx:dx+region.shape[1]] = region
 
-	return canvas, (ratio, (dx, dy))
+	return canvas, (zoom, targ, (x0, y0), (dx, dy))
 
 	# for frame in range()
 
 from scipy.ndimage import gaussian_filter as blur
 
 def create_heatmaps(coords, spec, size=46,dims=19):
-	ratio, (dx, dy) = spec
+	(zoom, targ, (x0, y0), (dx, dy)) = spec
 	canvas = np.zeros((size, size, dims))
 
 	for ii, coord in enumerate(coords):
@@ -133,17 +143,20 @@ def create_heatmaps(coords, spec, size=46,dims=19):
 			# invalid coord - DNE in image
 			continue
 
-		xx = int((dx + ratio * xx) * size/368)
-		yy = int((dy + ratio * yy) * size/368)
+		shrink = size / 368
+		rx = int((dx + (xx - x0) / zoom) * shrink)
+		ry = int((dy + (yy - y0) / zoom) * shrink)
 
-		if xx >= size or yy >= size:
+		if rx >= size or ry >= size:
+			continue
+		if rx < 0 or ry < 0:
 			continue
 
-		canvas[yy, xx, ii] = 1
+		canvas[ry, rx, ii] = 1
 		img = blur(canvas[:, :, ii], sigma=1)
 		canvas[:, :, ii] = img / np.max(img)
 
-		canvas[yy, xx, -1] = 1 # sum mask
+		canvas[ry, rx, -1] = 1 # sum mask
 
 	assert np.max(canvas[:, :, -1]) <= 1.0
 	img = blur(canvas[:, :, -1], sigma=1)
@@ -154,18 +167,24 @@ def create_heatmaps(coords, spec, size=46,dims=19):
 	return canvas
 
 def create_mask(bbox, spec, size=46, dims =19, coords=None, buffer=1):
-	ratio, (dx, dy) = spec
+	(zoom, targ, (x0, y0), (dx, dy)) = spec
 	canvas = np.zeros((size, size, dims))
 
-	x0, y0, xF, yF = bbox
-	x0 = int((dx + ratio * x0) * size/368)
-	xF = int((dx + ratio * xF) * size/368)
-	y0 = int((dy + ratio * y0) * size/368)
-	yF = int((dy + ratio * yF) * size/368)
+	shrink = size / 368
+	xS, yS, xF, yF = bbox
 
+	xS = int((dx + (xS - x0) / zoom) * shrink)
+	yS = int((dy + (yS - y0) / zoom) * shrink)
+
+	xF = int((dx + (xF - x0) / zoom) * shrink)
+	yF = int((dy + (yF - y0) / zoom) * shrink)
+
+	# print(spec)
+	# print(xS, xF, yS, yF)
+	# assert False
 	for ii in range(dims):
 		# canvas[y0:yF, x0:xF, ii] = 1
-		canvas[y0-buffer:yF+buffer, x0-buffer:xF+buffer, ii] = 1
+		canvas[max(yS-buffer, 0):yF+buffer, max(xS-buffer, 0):xF+buffer, ii] = 1
 
 	if coords is not None:
 		# zero out joints that are missing from PENN via masking
@@ -191,14 +210,15 @@ def next_video_batch(refs, bsize=6):
 	targets = []
 
 	for ref in brefs:
-		sized, specs = zip(*[size_image(imread(path)) for path in ref['frames']])
+		sized, specs = zip(*[size_image(imread(path), ref['boxes'][ii]) for ii, path in enumerate(ref['frames'])])
 
 		heats = [create_heatmaps(ref['coco_coords'][ii], specs[ii]) for ii in range(len(ref['frames']))]
 		mask = [create_mask(ref['boxes'][ii], specs[ii], coords=ref['coco_coords'][ii]) for ii in range(len(ref['frames']))]
 
 		# mask all except final
 		for ii in range(len(ref['frames'])):
-			heats[ii][:, :, :-1] = np.multiply(heats[ii][:, :, :-1], mask[ii][:, :, :-1])
+			# heats[ii][:, :, :-1] = np.multiply(heats[ii][:, :, :-1], mask[ii][:, :, :-1])
+			heats[ii][:, :, :] = np.multiply(heats[ii][:, :, :], mask[ii][:, :, :])
 
 		videos.append(sized)
 		masks.append(mask)
